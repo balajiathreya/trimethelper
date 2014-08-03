229d0abe02c0fa91e29c06ae07952fd3a4fd49a7
balajiathreya <athreya86@gmail.com>
2014-08-03 17:54:21 -0400

    added wsgi file and moved to a separate folder:

---
+++ trimetnotifier/README.md
@@ -0,0 +1,6 @@
+trimetnotifier
+==============
+
+Uses Twitter API to track Trimet and sends an email whenever there is a problem.
+
+Uses Flask - a Python micro web framework
---
+++ trimetnotifier/credentials.py
@@ -0,0 +1,15 @@
+# create a new application in dev.twitter.com and add your own api key and secret
+# also follow instructions https://dev.twitter.com/docs/auth/application-only-auth to create a bearertoken or use getBearerTokenFromTwitter 
+# function in trimetnotifier.py
+
+apikey=''
+apisecret=''
+bearertoken=''
+
+
+MAIL_SERVER='smtp.gmail.com'
+MAIL_PORT=465
+MAIL_USE_TLS = False
+MAIL_USE_SSL= True
+MAIL_USERNAME = 'gmail_email_id'
+MAIL_PASSWORD = 'password'
