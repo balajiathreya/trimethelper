6c79743a2cec8a79a2d992c5b68b133eff1dc673
Balaji Athreya <athreya86@gmail.com>
2014-08-02 21:55:53 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,7 @@
+# create a new application in dev.twitter.com and add your own api key and secret
+# also follow instructions https://dev.twitter.com/docs/auth/application-only-auth to create a bearertoken or use getBearerTokenFromTwitter 
+# function in trimetnotifier.py
+
 apikey=''
 apisecret=''
 bearertoken=''
